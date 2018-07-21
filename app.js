49892a8990efc1d8fea846f48414240391822525
Ermain <paulermain@gmail.com>
2018-07-21 19:31:07 -0400

    Second commit: Restructured the app the store the contact data in a json file. TODO: Implement a mongoose Schema for mongodb data storage and create the CRUD methods for the contact list

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 const os = require('os');
-const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
 const contacts = require('./contacts');
 
 var cmdargs = process.argv;
 var inputArgs = process.argv[2];
-console.log(cmdargs);
-console.log(inputArgs);
+// console.log(cmdargs);
+// console.log(inputArgs);
 
-if (inputArgs === 'list') {
+const nameOptions = {
+        describe: 'Contact Name',
+        demand: true,
+        alias: 'n'
+};
+
+const numberOptions = {
+        describe: 'Contact Number',
+        demand: true,
+        alias: 'num'
+};
+
+const argv = yargs
+                  .command('add', 'Add a new contact', {
+                      nameOptions,
+                      numberOptions
+                  })
+                  .command('list', 'List all contact')
+                  .command('read', 'Read a contact', {
+                      nameOptions
+                  })
+                  .command('remove', 'Remove a contact')
+                  .help()
+                  argv;
+
+var command = argv._[0];
+
+if (command === 'add') {
     console.log('Listing contacts');
     
-} else if(inputArgs === 'add') {
+} else if(command === 'list') {
     console.log('Adding contact...');
+}else if(command === 'read'){
+    console.log('Reading contacts');
+}else if(command === 'Remove'){
+    console.log('Removing command');   
 }else{
     console.log('Command not understood');
 }
